798b9deddabf7ab28ad05fdc54b41b066468e8c9
Hans Hinrichsen <hh@etcconnect.com>
2021-04-25 20:14:53 -0500

    Update syntax, made uniform timestamp format

--- include/pacer.h
+++ include/pacer.h
@@ -3,13 +3,13 @@
 #include <cstdint>
 #include <vector>
 #include <limits>
+#include <string>
 
 namespace Pacer
 {
     typedef uint8_t MessageType;
     typedef uint32_t TimelineId;
     constexpr TimelineId INVALID_TIMELINE_ID = std::numeric_limits<TimelineId>::max();
-    typedef uint64_t ReferenceClock; //tbd
 
     typedef std::vector<uint8_t> MESSAGE_BUFFER;
 
@@ -17,7 +17,7 @@ namespace Pacer
     {
         kPrototype = 0,
         kCurrent = kPrototype
-    }
+    };
 
     enum class MessageType : uint8_t
     {
@@ -26,7 +26,7 @@ namespace Pacer
         kPing = 2,
         kPong = 3,
         kCount
-    }
+    };
 
     enum class TimelineStatus : uint8_t
     {
@@ -35,12 +35,12 @@ namespace Pacer
         kRunning = 2,
         kPaused = 3,
         kCount
-    }
+    };
 
-    struct TimelineStamp
+    struct PacerTimestamp
     {
-        TimelineStamp() = default;
-        TimelineStamp(uint8_t hour, uint8_t minute, uint8_t second, uint16_t value, uint16_t scale)
+        PacerTimestamp() = default;
+        PacerTimestamp(uint8_t hour, uint8_t minute, uint8_t second, uint16_t value, uint16_t scale)
             : hour_(hour)
             , minute_(minute)
             , second_(second)
@@ -50,25 +50,25 @@ namespace Pacer
 
         uint8_t hour_ = 0;
         uint8_t minute_ = 0;
-        uint8_t seconds_ = 0;
+        uint8_t second_ = 0;
         uint16_t subsecondValue_ = 0;
         uint16_t subsecondScale_ = 1000;
     };
 
-    struct TimelineStatus
+    struct TimelineStatusPacket
     {
         Version version_ = Version::kPrototype;
         TimelineId timelineId_ = INVALID_TIMELINE_ID;
         uint32_t sequenceNumber_ = 0;
         TimelineStatus status_ = TimelineStatus::kStopped;
-        ReferenceClock referenceTimestamp_= 0;
+        PacerTimestamp referenceClock;
+        PacerTimestamp referenceTimeOffset;
+        PacerTimestamp timestamp_;
         std::string name_;
-        TimelineStamp referenceTimeOffset;
-        TimelineStamp timestamp_;
         uint8_t timelineHint_ = 0;
     };
-    void PackTimelineStatus(const TimelineStatus & message, MESSAGE_BUFFER & buf);
-    bool UnpackTimelineStatus(const MESSAGE_BUFFER & buf, TimelineStatus & message);
+    void PackTimelineStatusPacket(const TimelineStatusPacket & message, MESSAGE_BUFFER & buf);
+    bool UnpackTimelineStatusPacket(const MESSAGE_BUFFER & buf, TimelineStatusPacket & message);
 
     struct TimelineName
     {
@@ -85,7 +85,7 @@ namespace Pacer
         uint16_t pingId = 0;
         uint64_t originatingTime = 0;
         uint64_t responseTime = 0;
-    }
+    };
     void PackPing(const Ping & ping, MESSAGE_BUFFER & buf);
     void UnpackPing(const MESSAGE_BUFFER & buf, Ping & ping);
 
@@ -93,8 +93,8 @@ namespace Pacer
     {
         Version version_ = Version::kPrototype;
         uint16_t pingId = 0;
-        ReferenceClock referenceTimestamp_ = 0;
-    }
+        PacerTimestamp referenceTimestamp_;
+    };
     void PackPong(const Pong & ping, MESSAGE_BUFFER & buf);
     void UnpackPong(const MESSAGE_BUFFER & buf, Pong & ping);
 }
